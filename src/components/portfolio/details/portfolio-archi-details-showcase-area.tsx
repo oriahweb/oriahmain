2fdb1ac493859af4d816108da7bf776bab010530
oriahweb <rexellaboadu7@gmail.com>
2025-07-15 01:09:09 +0530

    image

--- src/components/portfolio/details/portfolio-archi-details-showcase-area.tsx
+++ src/components/portfolio/details/portfolio-archi-details-showcase-area.tsx
@@ -18,7 +18,7 @@ export default function PortfolioArchiDetailsShowcaseArea() {
           className="tp-showcase-details-bg d-flex align-items-center justify-content-center include-bg p-relative"
           style={{
             backgroundImage:
-              "url(/assets/img/inner-project/showcase/archi_inner.png)",
+              "url(/assets/img/inner-project/showcase/archi-inner.png)",
           }}
         >
           <div className="tp-showcase-details-scroll smooth">
